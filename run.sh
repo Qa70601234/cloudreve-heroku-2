4e4ca2bfff3fd75d0c62ef4e69e5157b25dea4eb
Qa70601234 <74185784+Qa70601234@users.noreply.github.com>
2021-09-02 23:18:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,11 +11,21 @@ Debug = false
 Server = 127.0.0.1:6379
 Password =
 DB = 0
+[Database]
+Type = $DB_Type
+Host = $DB_Host
+Port = $DB_Port
+User = $DB_User
+Password = $DB_Password
+Name = $DB_Name
+Charset = $DB_Charset
+TablePrefix = $DB_TablePrefix
+
 ; Session 密钥, 一般在首次启动时自动生成
 ; 5201314的md5加密密文为723d505516e0c197e42a6be3c0af910e
 ; 搭配cloudreve.db 默认关闭注册 管理员为 admin@cloudreve.org / cloudreve@2020
-SessionSecret = 723d505516e0c197e42a6be3c0af910e
+#SessionSecret = 723d505516e0c197e42a6be3c0af910e
 ; Hash 加盐, 一般在首次启动时自动生成
-HashIDSalt = 723d505516e0c197e42a6be3c0af910e
+#HashIDSalt = 723d505516e0c197e42a6be3c0af910e
 EOF
 (redis-server &) && (/root/cloudreve/cloudreve -c /root/cloudreve/mycloudreve.ini)
